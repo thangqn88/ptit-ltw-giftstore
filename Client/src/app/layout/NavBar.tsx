feb1685f83078794381c74d3e075f79ddc24973c
ThangQN <namthang.bkhn@gmail.com>
2025-01-18 23:40:13 +0700

    Enhance NavBar with navigation links and shopping cart icon; comment out font imports

--- Client/src/app/layout/NavBar.tsx
+++ Client/src/app/layout/NavBar.tsx
@@ -1,19 +1,90 @@
-import { DarkMode, LightMode } from "@mui/icons-material";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 type Props = {
   darkMode: boolean;
   toggleDarkMode: () => void;
 };
 
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
+
+const navStyles = {
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "gray.500",
+  },
+  "&.active": {
+    color: "secondary.main",
+  },
+};
+
+const boxStyles = { display: "flex", alignItems: "center" };
+
 export default function NavBar({ darkMode, toggleDarkMode }: Props) {
   return (
     <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6">Gift-Store</Typography>
-        <IconButton onClick={() => toggleDarkMode()}>
-          {darkMode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}
-        </IconButton>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={boxStyles}>
+          <Typography component={NavLink} to="/" variant="h6" sx={navStyles}>
+            Gift-Store
+          </Typography>
+          <IconButton onClick={() => toggleDarkMode()}>
+            {darkMode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}
+          </IconButton>
+        </Box>
+
+        <List sx={{ display: "flex" }}>
+          {midLinks.map((link) => (
+            <ListItem
+              key={link.path}
+              component={NavLink}
+              to={link.path}
+              sx={navStyles}
+            >
+              {link.title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <Box sx={boxStyles}>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map((link) => (
+              <ListItem
+                key={link.path}
+                component={NavLink}
+                to={link.path}
+                sx={navStyles}
+              >
+                {link.title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./app/layout/styles.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes/Routes";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+// import "@fontsource/roboto/300.css";
+// import "@fontsource/roboto/400.css";
+// import "@fontsource/roboto/500.css";
+// import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
